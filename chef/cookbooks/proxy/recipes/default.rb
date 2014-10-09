b68d980c6adffecdffd0c3f304a85aaa9fdcd3a6
Sebastian Trahm <basti@schleifi.com>
2014-10-09 22:41:10 +0200

    Get rid of unused http::Proxy
    Get rid of the http::Proxy template as we can switch
    to https://github.com/tmatilai/vagrant-proxyconf or
    the likes in case we are behind a proxy server.

--- chef/cookbooks/proxy/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-Chef::Log.info("[Adding: proxy configuration for apt(8)]")
-template "/etc/apt/apt.conf.d/01proxy" do
-  source "apt-proxy.config"
-  mode "0644"
-  owner "root"
-  group "root"
-  action :create_if_missing
-end
